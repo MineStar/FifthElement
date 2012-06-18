bf56bb514bb83e14c9118d949c70d0385604c563
Meldanor <Bhaals@gmx.de>
2012-06-18 12:01:44 +0200

    Initial commit
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/FifthElement/core/Core.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of FifthElement.
+ * 
+ * FifthElement is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * FifthElement is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FifthElement.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.FifthElement.core;
+
+import de.minestar.minestarlibrary.AbstractCore;
+
+public class Core extends AbstractCore {
+
+    public static final String NAME = "FifthElement";
+
+    public Core() {
+        super(NAME);
+    }
+}
