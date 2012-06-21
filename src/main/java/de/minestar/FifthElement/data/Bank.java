dc0110b3ba34edd143b52ddc4f885a6afc338cea
Meldanor <Bhaals@gmx.de>
2012-06-21 13:38:55 +0200

    Add datastructure for Banks
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/FifthElement/data/Bank.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of FifthElement.
+ * 
+ * FifthElement is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * FifthElement is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FifthElement.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.FifthElement.data;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import de.minestar.FifthElement.core.Core;
+import de.minestar.minestarlibrary.utils.ConsoleUtils;
+
+public class Bank {
+
+    private int id;
+    private String owner;
+    private Location location;
+
+    // CONSTRUCTOR WHEN PLAYER CREATES A HOME
+    public Bank(Player player) {
+        this.owner = player.getName();
+        this.location = player.getLocation();
+    }
+
+    // CONSTUCTOR WHEN HOME IS LOADED FROM DATABASE
+    public Bank(int id, String owner, double x, double y, double z, float yaw, float pitch, String worldName) {
+        this.id = id;
+        this.owner = owner;
+        setLocation(x, y, z, yaw, pitch, worldName);
+    }
+
+    private void setLocation(double x, double y, double z, float yaw, float pitch, String worldName) {
+        World w = Bukkit.getWorld(worldName);
+        if (w != null)
+            this.location = new Location(w, x, y, z, yaw, pitch);
+        else
+            ConsoleUtils.printError(Core.NAME, "Can't create a Bank for '" + this.owner + "' because the world '" + worldName + "' doesn't exist!");
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void updateLocation(Location loc) {
+        this.location = loc;
+    }
+
+    public void setId(int id) {
+        if (id == 0)
+            this.id = id;
+        else
+            ConsoleUtils.printError(Core.NAME, "Bank of '" + owner + "' has already an database id!");
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sBuilder = new StringBuilder("Bank: ");
+
+        sBuilder.append("owner= ");
+        sBuilder.append(owner);
+        sBuilder.append(", location=");
+        sBuilder.append(location);
+        sBuilder.append(", id=");
+        sBuilder.append(id);
+        return sBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Bank))
+            return false;
+
+        Bank that = (Bank) obj;
+        return this.owner.equals(that.owner) && this.location.equals(that.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.owner.hashCode() * this.location.hashCode() << 5;
+    }
+}
